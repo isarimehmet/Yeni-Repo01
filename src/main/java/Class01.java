91cf0dca9107cf81a1e6afd205999b7b7c23061e
isarimehmet <isarimehmet@yahoo.com>
2023-09-09 13:07:24 -0400

    versiyon1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,8 @@
+public class Class01 {
+    public static void main(String[] args) {
+        int y=1;
+        int x=2;
+        System.out.println(x+y);
+        System.out.println("Versiyon 1 icin islem tamam");
+    }
+}
