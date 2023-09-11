107a812ca4e443e563be39e3900b137d65512103
isarimehmet <isarimehmet@yahoo.com>
2023-09-11 03:50:59 -0400

    Versiyon 2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -4,5 +4,8 @@ public class Class01 {
         int x=2;
         System.out.println(x+y);
         System.out.println("Versiyon 1 icin islem tamam");
+        // Versiyon 2 calismalari
+        int z = 4;
+        System.out.println(x+z+y);
     }
 }
